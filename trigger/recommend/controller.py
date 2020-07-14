f744161d3a31253a31f3e5a5cdc686a98794ffa6
David Pinto <m44094@alunos.uevora.pt>
2020-07-15 00:55:35 +0100

    Added new funcionalities to controller

--- trigger/recommend/controller.py
+++ trigger/recommend/controller.py
@@ -1,31 +1,44 @@
 from typing import List, Optional
 
-from trigger.models.User import User
-from trigger.models.Opening import Opening
+from trigger.models.user import User
 from trigger.models.Match import Match
+from trigger.recommend import smart
 
-# configs?
-# or class?
-from trigger.recommend.smart import Smart
-
-lowest_to_consider_match = 0.
+from trigger.recommend.clusters import Clusters
+from trigger.recommend.user_transformer import UserTransformer
+from trigger.recommend.opening_transformer import OpeningTransformer
 
 
 class Controller:
-    def __init__(self, smart: Smart) -> None:
-        self.smart = smart
+    def __init__(self, cluster: Clusters, user_transformer: UserTransformer, ) -> None:
+        self.clusters = cluster
+        self.user_transformer = user_transformer
 
-    def on_user_change(self, user: User, openings: List[Opening]) -> Optional[List[Match]]:
+    def user_matches(self, user: User, lowest_distance: float = 0.5) -> List[Match]:
         # very complex stuff
+
+        good_clusters = []
+        representatives = self.clusters.representatives()
+        user_point = self.user_transformer.transform(user)
+
+        for cluster_instance in representatives:
+
+            score = smart.user_distance(user, user_point, cluster_instance.opening, cluster_instance.point)
+            if score < lowest_distance:
+                continue
+
+            good_clusters.append(cluster_instance.cluster_number)
 
         matches = []
 
-        for opening in openings:
+        for cluster_number in good_clusters:
+            cluster_instances = self.clusters.get_cluster(cluster_number).instances
+            for cluster_instance in cluster_instances:
 
-            score = self.smart.distance(user, opening)
-            if score < lowest_to_consider_match:
-                continue
+                score = smart.user_distance(user, user_point, cluster_instance.opening, cluster_instance.point)
+                if score < lowest_distance:
+                    continue
 
-            matches.append(Match(user, score, opening))
+                matches.append(Match(user, score, cluster_instance.opening))
 
         return matches
