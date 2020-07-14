78a3bd3414d1fa0952a3dbf82952ceb3a4c4e6e1
David Pinto <m44094@alunos.uevora.pt>
2020-07-15 00:55:48 +0100

    Modified main so it works with new examples

--- main.py
+++ main.py
@@ -1,27 +1,69 @@
-from trigger.models.Opening import Opening
-from trigger.models.Softskill import Softskill
-from trigger.models.Hardskill import Hardskill
-from trigger.models.Language import Language
-from trigger.models.User import User
+from trigger.models.opening import Opening
+from trigger.models.softskill import Softskill
+from trigger.models.hardskill import Hardskill
+from trigger.models.language import Language
+from trigger.models.user import User
+from trigger.recommend import smart
 from trigger.recommend.controller import Controller
-import trigger.train
+from trigger.recommend.clusters import Clusters
+from trigger.recommend.opening_transformer import OpeningTransformer
+from trigger.recommend.skill_transformers.soft_skill_transformer import SoftskillTransformer
 
-# on user change
-from trigger.recommend.smart import Smart
+import pprint
 
-smart = Smart("")
-controller = Controller(smart)
+# some init?
+from trigger.recommend.user_transformer import UserTransformer
+
+all_softskill_names = ["Leadership", "Creativity", "Individuality"]
+softskill_transformer = SoftskillTransformer(all_softskill_names)
+
+opening_transformer = OpeningTransformer(softskill_transformer)
+clusters = Clusters(opening_transformer)
+
+user_transformer = UserTransformer(softskill_transformer)
+
+controller = Controller(clusters, user_transformer)
 
 # We got this from the web?
 
-user = User(softSkills=[Softskill("Bullshitting", 3)],
-            hardSkills=[Hardskill("Please", 1)], interests=["Swimming"])
-openings = [Opening(sector='Color', area="Klawn", languages=[Language("English", 10)], softSkills=[Softskill("Bullshitting", i + 1), Softskill(
-    "Gestão", 5 - i + 1)], hardSkills=[Hardskill("Please", i + 1)]) for i in range(5)]
+user = User(softSkills=[Softskill("Leadership", 2), Softskill("Creativity", 3)],
+            hardSkills=[], interests=["Swimming"])
 
-matches = controller.on_user_change(user, openings)
+openings = \
+    [
+        Opening(
+            entityId="0x" + str(i),
+            sector='Cheese',
+            area="Informática",
+            languages=[Language("English")],
+            softSkills=[Softskill("Leadership", i + 1), Softskill("Individuality", 5 - i)],
+            hardSkills=[]
+        )
+        for i in range(5)
+    ]
 
-print(user)
+openings.append(
+    Opening(
+            entityId="0x6",
+            sector='Cheese',
+            area="Informática",
+            languages=[Language("English")],
+            softSkills=[Softskill("Leadership", 1), Softskill("Creativity", 2)],
+            hardSkills=[]
+    )
+)
 
-for match in matches:
-    print(match.opening, "<Score: " + str(match.score) + ">")
+for opening in openings:
+    clusters.add_opening(opening)
+
+print("Clusters:")
+pprint.pprint(clusters.cluster_dict)
+
+print("User: ")
+pprint.pprint(user)
+
+matches = controller.user_matches(user, 0)
+
+print("Matches: ")
+
+pprint.pprint([(match.opening, match.score) for match in matches])
