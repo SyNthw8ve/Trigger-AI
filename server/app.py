8c201316706a85f9d32e9f3facf3e56266a0bc18
David Pinto <daxtery11@gmail.com>
2020-09-22 19:40:43 +0100

    Server: start of compute_user_matches()
    The server can fetch user information
    from the mongodb database and calculate
    matches locally.
    Still need to save them though
    
    Also add the need for a .json configure object
    to (configure) the connection (and gitignore it)
    -> this can be seen in action with example.json

--- server/app.py
+++ server/app.py
@@ -1,37 +1,110 @@
+from flask import Flask
+from rq import Queue
+from scipy.spatial.distance import cosine
+from server.database.user_model import UserModel
+from trigger.train.transformers.input_transformer import SentenceEmbedder
+from trigger.train.cluster.gstream.gstream import GNG
+import os
+from trigger.models.match import Match
+from typing import Any, List
 import pymongo
 import redis
 import time
 
-from rq import Queue
-from flask import Flask
+import json
+import sys
+from pathlib import Path
+
+from trigger.train.cluster.gstream.node import Node
+from trigger.train.transformers.user_transformer import UserInstance
+
+sys.path.append(str(Path('.').absolute().parent))
+sys.path.append(str(Path('..').absolute().parent))
 
 app = Flask(__name__)
 
-@app.route('/user_match/<user_id>', methods=['POST'])
-def compute_user_matches(user_id):
+clusterer = GNG(epsilon_b=0.001,
+                epsilon_n=0,
+                lam=5,
+                beta=0.9995,
+                alpha=0.95,
+                max_age=10,
+                off_max_age=10,
+                lambda_2=0.2,
+                nodes_per_cycle=1,
+                dimensions=1024,
+                index_type='L2')
 
-    pass
+sentence_embedder = SentenceEmbedder()
+
+score_to_be_match = 0.5
+
+config_path = "server/config.json"
+
+with open(config_path) as f:
+    config = json.load(f)
+
+# FIXME: when using mongodb+srv:// URIs for host it needs to have dnspython
+client = pymongo.MongoClient(config["host"])
+database = client[config["database"]]
+
+
+def computeScore(user_instance: Any, opening_instance: Any) -> float:
+    return 1 - cosine(user_instance, opening_instance)
+
+
+@app.route('/user_match/<user_id>', methods=['POST'])
+def compute_user_matches(user_id: str):
+    user = UserModel.get_user_data(user_id, database)
+
+    # TODO: cache user instance?
+    user_instance = UserInstance(user, sentence_embedder)
+
+    would_be_cluster_id = clusterer.predict(user_instance.embedding)
+
+    instances, tags = clusterer.get_instances_and_tags_in_cluster(
+        would_be_cluster_id)
+
+    matches = [
+        Match(
+            user_id,
+            computeScore(user, instance),
+            tag
+        )
+        for instance, tag in zip(instances, tags)
+    ]
+
+    good_matches = [
+        match
+        for match in matches
+        if match.score > score_to_be_match
+    ]
+
+    print(good_matches)
+    # TODO: save matches
+
+    return str(good_matches)
+
 
 @app.route('/user_match/<user_id>', methods=['PUT'])
-def update_user_matches(user_id):
+def update_user_matches(user_id: str):
 
     pass
+
 
 @app.route('/opening/<opening_id>', methods=['POST'])
-def insert_opening_to_cluster(opening_id):
+def insert_opening_to_cluster(opening_id: str):
 
     pass
+
 
 @app.route('/opening/<opening_id>', methods=['PUT'])
-def update_opening(opening_id):
+def update_opening(opening_id: str):
 
     pass
+
 
 @app.route('/opening/<opening_id>', methods=['DELETE'])
-def remove_opening_from_cluster(opening_id):
+def remove_opening_from_cluster(opening_id: str):
 
     pass
-
-if __name__ == "__main__":
-
-    client = pymongo.MongoClient('localhost', 27017)
