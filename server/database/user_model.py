a9e1166e4904c925b849825f6a5aa9569928d232
David Pinto <daxtery11@gmail.com>
2020-09-22 19:36:09 +0100

    UserModel can now fetch user_data

--- server/database/user_model.py
+++ server/database/user_model.py
@@ -1,14 +1,49 @@
-import pymongo
+from trigger.models.hardskill import Hardskill
+from trigger.models.softskill import Softskill
+from trigger.models.user import User
+
+from server import database
 
 from typing import List
-from trigger.models.Match import Match
+from pymongo.database import Database
 
-class UserModel():
+
+from server.database.names import hardskills_collection_name, softskills_collection_name
+
+from pymongo.mongo_client import MongoClient
+from trigger.models.match import Match
+
+from bson.objectid import ObjectId
+from bson.dbref import DBRef
+
+class UserModel:
+
+    collection_name = "users"
 
     @staticmethod
-    def get_user_data(user_id: str, db):
+    def get_user_data(user_id: str, db: Database) -> User:
+        users_collection = db[UserModel.collection_name]
+        user_from_db = users_collection.find_one({"_id": ObjectId(user_id)})
 
-        pass
+        softskills = []
+        key = "softSkills"
+
+        if key in user_from_db:
+            softskills_collection = db[softskills_collection_name]
+            for ss_ref in user_from_db[key]:
+                softskill_from_db = softskills_collection.find_one({"_id": ObjectId(ss_ref)})
+                softskills.append(Softskill(name=softskill_from_db["name"], score=0))
+
+        hardskills = []
+        key = "hardSkills"
+
+        if key in user_from_db:
+            hardskills_collection = db[hardskills_collection_name]
+            for hs_ref in user_from_db[key]:
+                hardskill_from_db = hardskills_collection.find_one({"_id": ObjectId(hs_ref)})
+                hardskills.append(Hardskill(name=hardskill_from_db["name"]))
+
+        return User(user_from_db["name"], softskills, hardskills, [])
 
     @staticmethod
     def insert_user_matches(user_id: str, matches: List[Match]):
