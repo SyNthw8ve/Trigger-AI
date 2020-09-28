6b937a40f336bcd18ca66898fe7a8dd1e1d1a8fa
David Pinto <daxtery11@gmail.com>
2020-09-28 22:04:14 +0100

    Separated jobs to a different file

---
+++ server/jobs.py
@@ -0,0 +1,27 @@
+from server.trigger_driver import TriggerDriver
+from server.trigger_worker import get_driver
+
+def on_compute_user_matches(user_id:str):
+    driver = get_driver()
+    driver.compute_user_matches(user_id)
+
+def on_compute_user_score(user_id: str, opening_id: str):
+    driver = get_driver()
+    return driver.compute_user_score(user_id, opening_id)
+
+def on_update_user_matches(user_id: str):
+    driver = get_driver()
+    driver.update_user_matches(user_id)
+
+def on_insert_opening_to_cluster(opening_id: str):
+    driver = get_driver()
+    driver.insert_opening_to_cluster(opening_id)
+
+
+def on_update_opening(opening_id: str):
+    driver = get_driver()
+    driver.update_opening(opening_id)
+
+def on_remove_opening_from_cluster(opening_id: str):
+    driver = get_driver()
+    driver.remove_opening_from_cluster(opening_id)
