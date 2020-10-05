0ee99d8c8efee40a419b464a42bb6dba22f70d90
David Pinto <daxtery11@gmail.com>
2020-10-05 17:06:02 +0100

    Remove no longer used fields from readers

--- util/readers/reader.py
+++ util/readers/reader.py
@@ -76,12 +76,12 @@ class DataReaderUsers:
                         ",") if s_t.strip() != '']
 
                     user_softskills = [
-                        Softskill(name=softskill, score=0) for softskill in softskills]
+                        Softskill(name=softskill) for softskill in softskills]
                     user_hardskills = [
                         Hardskill(name=hardskill) for hardskill in hardskills]
 
                     user = User(name=username, hardSkills=user_hardskills,
-                                softSkills=user_softskills, interests=["Eating"])
+                                softSkills=user_softskills)
 
                     username = ''
                     hardskills = []
@@ -142,13 +142,13 @@ class DataReaderOpenings:
                         ",") if s_t.strip() != '']
 
                     opening_softskills = [
-                        Softskill(name=softskill, score=0) for softskill in softskills]
+                        Softskill(name=softskill) for softskill in softskills]
 
                     opening_hardskills = [
                         Hardskill(name=hardskill) for hardskill in hardskills]
 
-                    opening = Opening(entityId=len(openings), sector=sector, area=area, hardSkills=opening_hardskills,
-                                      softSkills=opening_softskills, languages=[Language(name="English")])
+                    opening = Opening(entityId=str(len(openings)), hardSkills=opening_hardskills,
+                                      softSkills=opening_softskills)
 
                     openings.append(opening)
 
