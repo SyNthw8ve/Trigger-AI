606a9a006d865bea0a7d26b1278a214eb8be70ec
David Pinto <daxtery11@gmail.com>
2020-10-09 02:20:09 +0100

    update find_best_scored to handle other measures

--- find_best_scored.py
+++ find_best_scored.py
@@ -1,18 +1,7 @@
 import os
 import json
 import sys
-from typing import Optional, Tuple
 
-TEST_RESULTS = Tuple[float, float, Optional[float], str]
-
-def look_at_composite_score(scores: TEST_RESULTS) -> float:
-    return 0 if scores[2] is None else scores[2] 
-
-def look_at_ss_score(scores: TEST_RESULTS) -> float:
-    return scores[0]
-
-def look_at_chs_score(scores: TEST_RESULTS) -> float:
-    return scores[1]
 
 def look_folder_and_sub_folders(directory_path: str) -> None:
     path_list = [
@@ -22,7 +11,7 @@ def look_folder_and_sub_folders(directory_path: str) -> None:
         if filename.endswith('.json')
     ]
 
-    all_scores = []
+    all_scores = {}
 
     for path in path_list:
         try:
@@ -35,39 +24,67 @@ def look_folder_and_sub_folders(directory_path: str) -> None:
                 mr = json_as_dict["results"].get("matches_results", None)
 
                 if mr is None:
-                    all_scores.append((
-                        float(json_as_dict["results"]["ss"]),
-                        float(json_as_dict["results"]["chs"]),
-                        None,
-                        path
-                    ))
-                    continue
-
-                all_scores.append((
-                    float(json_as_dict["results"]["ss"]),
-                    float(json_as_dict["results"]["chs"]),
-                    float(mr["composite score"]),
-                    path
-                ))
+                    all_scores[path] = {
+                        "ss": float(json_as_dict["results"]["ss"]),
+                        "chs": float(json_as_dict["results"]["chs"]),
+                        r"% at least 1 match": 0,
+                        "avg #matches": 0,
+                        "max #matches": 0,
+                        "avg #potential": 0,
+                        "max #potential": 0,
+                        "avg matches score": 0,
+                        "avg similarity score": 0,
+                        "avg quality score": 0,
+                    }
+                else:
+                    all_scores[path] = {
+                        "ss": float(json_as_dict["results"]["ss"]),
+                        "chs": float(json_as_dict["results"]["chs"]),
+                        r"% at least 1 match": float(mr[r"% at least 1 match"], ),
+                        "avg #matches": float(mr["avg #matches"]),
+                        "max #matches": float(mr["max #matches"]),
+                        "avg #potential": float(mr["avg #potential"]),
+                        "max #potential": float(mr["max #potential"]),
+                        "avg matches score": float(mr["avg matches score"]),
+                        "avg similarity score": float(mr["avg similarity score"]),
+                        "avg quality score": float(mr["avg quality score"]),
+                    }
 
         except Exception as e:
             print(e, path)
 
-    n = 10
+    n = 100
 
-    descriptions_and_functions = [
-        ("ss", look_at_ss_score),
-        ("chs", look_at_chs_score),
-        ("composite", look_at_composite_score)
+    keys = [
+        "ss",
+        "chs",
+        r"% at least 1 match",
+        "avg #matches",
+        "max #matches",
+        "avg #potential",
+        "max #potential",
+        "avg matches score",
+        "avg similarity score",
+        "avg quality score",
     ]
 
-    for description, function in descriptions_and_functions:
-        print(f"By {description} score the first {n} are:")
-        ordered = sorted(all_scores, key=function, reverse=True)[0:n]
+    results = {}
 
-        for test in ordered:
-            print(test)
+    for key in keys:
+        try:
+            ordered = sorted(all_scores.items(), key=lambda item: item[1].get(key, 0), reverse=True)[0:n]
+            results[key] = [
+                {
+                    "path": path,
+                    "scores": scores
+                } for path, scores in ordered
+            ]
 
+        except Exception as e:
+            print(e, key)
+
+    with open('best.json', 'w') as f:
+        json.dump(results, f)
 
 
 if __name__ == "__main__":
