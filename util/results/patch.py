68cf706cf01f3496cbe59cfd2bd9daab441fbb45
David Pinto <daxtery11@gmail.com>
2020-10-13 23:21:17 +0100

    We can reorder

--- util/results/patch.py
+++ util/results/patch.py
@@ -1,8 +1,39 @@
 import json
 import statistics
-from typing import List, Callable, Optional
+from collections import Counter
+from typing import List, Callable, Optional, Dict, Any
 
-from util.metrics.matches import average_from_distribution, max_from_distribution, min_from_distribution
+from util.metrics.matches import average_from_distribution, max_from_distribution, min_from_distribution, \
+    make_distribution, to_range
+
+default_order = [
+    "distribution #matches",
+    "distribution #potential",
+    "distribution avg similarity range",
+    "distribution avg quality range",
+    "distribution avg real range",
+    "distribution avg matches range",
+    "distribution matches range",
+    "% at least 1 match",
+    "avg #matches",
+    "max #matches of a user",
+    "min #matches of a user",
+    "avg #potential",
+    "max #potential of a user",
+    "min #potential of a user",
+    "avg matches score",
+    "avg real score",
+    "avg similarity score",
+    "avg quality score",
+    "by_user",
+]
+
+
+def reorder_dictionary(order: List[str], dictionary: Dict[str, Any]) -> Dict[str, Any]:
+    return {
+        key: dictionary[key]
+        for key in order
+    }
 
 
 def rename(json_dict: dict) -> Optional[dict]:
@@ -17,32 +48,37 @@ def rename(json_dict: dict) -> Optional[dict]:
         return None
 
     rename_map = {
-        "distribution similarity": "distribution avg similarity range",
-        "distribution quality": "distribution avg quality range",
-        "distribution real": "distribution avg real range",
-        "distribution matches": "distribution avg matches range",
+        "max #matches of a user": "max #matches",
+        "min #matches of a user": "min #matches",
+        "max #potential of a user": "max #potential",
+        "min #potential of a user": "min #potential",
     }
 
-    key_order = [
-        rename_map.get(key, key)
-        for key in matches_results
-    ]
-
-    for old_name, new_name in rename_map.items():
-
+    for new_name, old_name in rename_map.items():
         matches_results[new_name] = matches_results[old_name]
         del matches_results[old_name]
 
-        if new_name in json_dict:
-            print("!")
-            exit(0)
+    return json_dict
 
-    new_dict = {
-        key: matches_results[key]
-        for key in key_order
-    }
 
-    results_dict["matches_results"] = new_dict
+def reorder(json_dict: dict) -> Optional[dict]:
+    results_dict = json_dict.get("results", None)
+
+    if results_dict is None:
+        return None
+
+    matches_results = results_dict.get("matches_results", None)
+
+    if matches_results is None:
+        return None
+
+    order = default_order.copy()
+
+    for key in matches_results:
+        if key not in order:
+            order.append(key)
+
+    results_dict["matches_results"] = reorder_dictionary(order, matches_results)
     return json_dict
 
 
@@ -78,6 +114,9 @@ def add_avg_max_min(json_dict: dict) -> Optional[dict]:
     if matches_results is None:
         return None
 
+    if "avg #matches" in matches_results:
+        return None
+
     matches_results["avg #matches"] = average_from_distribution(matches_results["distribution #matches"])
     matches_results["max #matches"] = max_from_distribution(matches_results["distribution #matches"])
     matches_results["min #matches"] = min_from_distribution(matches_results["distribution #matches"])
@@ -93,11 +132,28 @@ def add_avg_max_min(json_dict: dict) -> Optional[dict]:
     matches_results["avg similarity score"] = statistics.mean([user["avg similarities"] for user in by_user])
     matches_results["avg quality score"] = statistics.mean([user["avg qualities"] for user in by_user])
 
-    # this reorders
-    del matches_results["by_user"]
-    matches_results["by_user"] = by_user
+    return json_dict
 
-    json_dict["matches_results"] = matches_results
+
+def add_matches_distribution(json_dict: dict) -> Optional[dict]:
+    results_dict = json_dict.get("results", None)
+
+    if results_dict is None:
+        return None
+
+    matches_results = results_dict.get("matches_results", None)
+
+    if matches_results is None:
+        return None
+
+    matches_range_counter = Counter()
+
+    for user_and_matches in matches_results["by_user"]:
+        for match in user_and_matches["matches"]:
+            matches_range_counter.update([to_range(match["score"], 5)])
+
+    matches_results["distribution matches range"] = {range_: count for range_, count in
+                                                     matches_range_counter.most_common()}
 
     return json_dict
 
@@ -109,9 +165,6 @@ def patch_result_paths(path_list: List[str],
     for path in path_list:
         with open(path, 'r') as file:
             json_as_dict = json.load(file)
-
-            if json_as_dict.get("results", None) is None:
-                continue
 
             patched_json = patch_function(json_as_dict)
 
