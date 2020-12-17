582198d92e28bde08d3abaa46a825ce7ff3fc3fb
David Pinto <daxtery11@gmail.com>
2020-12-17 13:49:15 +0000

    fix ecm_tests

--- tests/ecm_tests.py
+++ tests/ecm_tests.py
@@ -2,11 +2,9 @@ import logging
 import os
 
 import pathlib
-import sys
-
-if __name__ == "__main__":
-    sys.path.append("..")
-    sys.path.append(".")
+from trigger_project.transformers.user_transformer import UserTransformer
+from trigger.train.transformers.sentence_embedder import SentenceEmbedder
+from trigger_project.transformers.opening_transformer import OpeningTransformer
 
 from trigger.train.cluster.ecm.ecm import ECM
 
@@ -22,8 +20,11 @@ logger.setLevel(logging.INFO)
 users_path = './examples/openings_users/users'
 openings_path = './examples/openings_users/openings'
 
-instances_paths = ['./data/instances',
-                   './data/instances_ss_confirmed', './data/save']
+instances_paths = [
+    './data/instances',
+    './data/instances_ss_confirmed',
+    './data/save'
+]
 
 
 def from_instance_path_to_layer_name(instance_path: str) -> str:
@@ -61,17 +62,17 @@ def test_ecm_matches_instances():
             if os.path.isfile(os.path.join(instances_path, f))
         ]
 
-        users_instances_files = [
+        users_instances_files = sorted([
             instance_path
             for instance_path in instances_files
             if  instance_path.find("users") != -1
-        ]
+        ])
 
-        openings_instances_files = [
+        openings_instances_files = sorted([
             instance_path
             for instance_path in instances_files
             if instance_path.find("openings") != -1
-        ]
+        ])
 
         for users_instances_path, openings_instances_path in zip(users_instances_files, openings_instances_files):
             logger.info("Doing users + openings instances @ %s, %s",
@@ -82,8 +83,7 @@ def test_ecm_matches_instances():
 
             instances_folder_name = pathlib.PurePath(instances_path).name
 
-            layer_folder = from_instance_path_to_layer_name(
-                openings_instances_path)
+            layer_folder = from_instance_path_to_layer_name(openings_instances_path)
 
             gng_tester = TestRunnerMatches(ECM, param_grid, openings_instances, users_instances,
                                            f'./results/openings_users/{instances_folder_name}/{layer_folder}/ECM',
@@ -103,16 +103,17 @@ def test_ecm_operations():
 
         instances_files = [os.path.join(folder_path, f) for f in os.listdir(folder_path)]
 
-        users_instances_files = [instance_path for instance_path in instances_files if
-                                 instance_path.find("users") != -1]
+        users_instances_files = sorted([instance_path for instance_path in instances_files if
+                                 instance_path.find("users") != -1])
 
-        operations_files = [instance_path for instance_path in instances_files if
-                            instance_path.find("operations") != -1]
+        operations_files = sorted([instance_path for instance_path in instances_files if
+                            instance_path.find("operations") != -1])
 
         for users_instances_path, operations_instances_path in zip(users_instances_files, operations_files):
             logger.info("Doing users + operations @ %s, %s", users_instances_path, operations_instances_path)
 
             operations = read_operations(operations_instances_path)
+            
             users_instances = DataInitializer.read_users(users_instances_path, users_path)
 
             instances_folder_name = pathlib.PurePath(folder_path).name
