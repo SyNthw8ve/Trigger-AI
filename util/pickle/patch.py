08c70b2c2c57975c5802d85a0a0f9783e698719b
David Pinto <daxtery11@gmail.com>
2020-12-17 02:17:55 +0000

    use util/pickle to update pickles

--- util/pickle/patch.py
+++ util/pickle/patch.py
@@ -1,12 +1,10 @@
 import os
 import logging
+from typing import List
 
-from trigger.train.transformers.user_transformer import UserInstance
-from trigger.train.transformers.opening_transformer import OpeningInstance
+from trigger_project.instances.opening_instance import OpeningInstance
+from trigger_project.instances.user_instance import UserInstance
 
-from trigger.models.softskill import Softskill as Softzkill
-from trigger.models.user import User as Userz
-from trigger.models.opening import Opening as Openingz
 
 import io
 import pickle
@@ -15,14 +13,23 @@ import pickle
 class RenameUnpickler(pickle.Unpickler):
     def find_class(self, module, name):
         renamed_module = module
-        if module == "trigger.models.SoftSkill":
-            renamed_module = "trigger.models.softskill"
-        elif module == "trigger.models.softskillz":
-            renamed_module = "trigger.models.softskill"
-        elif module == "trigger.models.userz":
-            renamed_module = "trigger.models.user"
-        elif module == "trigger.models.openingz":
-            renamed_module = "trigger.models.opening"
+        if module == "trigger.models.softskill":
+            renamed_module = "trigger_project.models.softskill"
+        elif module == "trigger.models.hardskill":
+            renamed_module = "trigger_project.models.hardskill"
+        elif module == "trigger.models.user":
+            renamed_module = "trigger_project.models.user"
+        elif module == "trigger.models.opening":
+            renamed_module = "trigger_project.models.opening"
+        elif module == "trigger.models.project":
+            renamed_module = "trigger_project.models.project"
+        elif module == "trigger.train.transformers.user_transformer":
+            renamed_module = "trigger_project.instances.user_instance"
+        elif module == "trigger.train.transformers.opening_transformer":
+            renamed_module = "trigger_project.instances.opening_instance"
+        elif module == "util.operation":
+            renamed_module = "trigger_project.operation"
+        
 
         return super(RenameUnpickler, self).find_class(renamed_module, name)
 
@@ -49,7 +56,7 @@ def renamed_loads(pickled_bytes):
 
 # Run this again
 
-def rename(instances_paths):
+def rename_instances(instances_paths: List[str]):
     for instances_path in instances_paths:
 
         instances_files = [
@@ -77,16 +84,44 @@ def rename(instances_paths):
             with open(users_instances_path, 'rb') as file:
                 users_instances = renamed_load(file)
 
-            for users_instance in users_instances:
-                old = users_instance.user
-                old_sss = old.softSkills
+            UserInstance.save_instances(users_instances_path, users_instances)
 
-                new_ss = [
-                    Softzkill(old_ss.name)
-                    for old_ss in old_sss
-                ]
+        for openings_instances_path in openings_instances_files:
 
-                users_instance.user = Userz(old.name, new_ss, old.hardSkills)
+            logging.info("Opening instances " + openings_instances_path)
+
+            with open(openings_instances_path, 'rb') as file:
+                openings_instances = renamed_load(file)
+
+            OpeningInstance.save_instances(openings_instances_path, openings_instances)
+
+def rename_operations(operations_folder: str):
+    for operations_test in os.listdir(operations_folder):
+
+        instances_files = [
+            os.path.join(os.path.join(operations_folder, operations_test, f))
+            for f in os.listdir(os.path.join(operations_folder, operations_test))
+            if os.path.isfile(os.path.join(operations_folder, operations_test, f))
+        ]
+
+        users_instances_files = [
+            instance_path
+            for instance_path in instances_files
+            if instance_path.find("users") != -1
+        ]
+
+        openings_instances_files = [
+            instance_path
+            for instance_path in instances_files
+            if instance_path.find("openings") != -1
+        ]
+
+        for users_instances_path in users_instances_files:
+
+            logging.info("Users instances " + users_instances_path)
+
+            with open(users_instances_path, 'rb') as file:
+                users_instances = renamed_load(file)
 
             UserInstance.save_instances(users_instances_path, users_instances)
 
@@ -97,17 +132,17 @@ def rename(instances_paths):
             with open(openings_instances_path, 'rb') as file:
                 openings_instances = renamed_load(file)
 
-            for opening_instance in openings_instances:
-                old = opening_instance.opening
-                old_sss = old.softSkills
-                new_ss = [
-                    Softzkill(old_ss.name)
-                    for old_ss in old_sss
-                ]
-                opening_instance.opening = Openingz(old.entityId, old.hardSkills, new_ss)
-
             OpeningInstance.save_instances(openings_instances_path, openings_instances)
 
+def rename_projects(projects_folder: str):
+
+    for projects_path in os.listdir(projects_folder):
+
+        with open(os.path.join(projects_folder, projects_path), 'rb') as file:
+            projects = renamed_load(file)
+
+        with open(os.path.join(projects_folder, projects_path), 'wb') as file:
+            pickle.dump(projects, file)
 
 def correct_entity_id(instances_paths):
     for instances_path in instances_paths:
