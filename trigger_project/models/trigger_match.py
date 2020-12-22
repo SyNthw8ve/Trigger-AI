a1db389d96c62d9b8f8c041784ba2479ff30cd66
David Pinto <daxtery11@gmail.com>
2020-12-22 19:25:33 +0000

    Remove unused Match

--- trigger_project/models/trigger_match.py
+++
@@ -1,9 +0,0 @@
-from trigger.match import Match
-from .user import User
-from .opening import Opening
-from dataclasses import dataclass
-
-@dataclass()
-class TriggerMatch(Match):
-    user: User
-    opening: Opening
