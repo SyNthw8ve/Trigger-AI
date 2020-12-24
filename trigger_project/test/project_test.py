7ef9e60212249c96ea42de011d14e2575a81abbd
David Pinto <daxtery11@gmail.com>
2020-12-24 12:18:11 +0000

    let's use the already defined json encoder

--- trigger_project/test/project_test.py
+++ trigger_project/test/project_test.py
@@ -3,7 +3,6 @@ import json
 from typing import List
 from ..models.project import Project
 from ..metrics.project import eval_variability, eval_variability_mahalanobis, eval_variability_cosine, eval_variability_cosine_to_mean
-from ..util.json_util.json_converter import project_to_json
 
 
 def test_variability(projects: List[Project]):
@@ -17,12 +16,16 @@ def test_variability(projects: List[Project]):
         score_kur = eval_variability(project)
         score_cos_mean = eval_variability_cosine_to_mean(project)
 
-        score = {'cosine': score_cos, 'cosine_to_mean': score_cos_mean, 'mahalanobis': score_maha,
-                 'kurtosis': score_kur["kurtosis"]}
+        json_project = {
+            "project": project,
+            'cosine': score_cos,
+            'cosine_to_mean': score_cos_mean,
+            'mahalanobis': score_maha,
+            'kurtosis': score_kur["kurtosis"]
+        }
 
-        json_project = project_to_json(project, score)
         projects_json.append(json_project)
 
     with open("./project_test.json", "w") as f:
-        from ..util.json_util.json_converter import EnhancedJSONEncoder
+        from trigger.util.json_encoder import EnhancedJSONEncoder
         json.dump(projects_json, f, cls=EnhancedJSONEncoder)
