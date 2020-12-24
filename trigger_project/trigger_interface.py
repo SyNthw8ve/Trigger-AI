7df51f5a22eab1c93e1593adcbd7ca29152f15fd
David Pinto <daxtery11@gmail.com>
2020-12-24 14:14:16 +0000

    TriggerInterface

---
+++ trigger_project/trigger_interface.py
@@ -0,0 +1,14 @@
+from trigger_project.scoring import TriggerScoring
+from trigger_project.evaluation.matches import eval_matches
+from typing import List, cast
+from trigger.interface import Interface
+from trigger.operations import CalculateMatchesInfo
+
+class TriggerInterface(Interface):
+
+    def _evaluate_matches_inner(self, values: List[CalculateMatchesInfo]):
+
+        instances, scorings = self._calculate_operation_matches_inner(values)
+        # Don't worry, they are trigger scorings :)
+        scorings = cast(List[List[TriggerScoring]], scorings)
+        return eval_matches(instances, scorings)
