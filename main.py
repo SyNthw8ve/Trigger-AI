2a82e590d462e84ef2f56843a0515cebd8835117
David Pinto <daxtery11@gmail.com>
2020-12-24 14:14:40 +0000

    working minimal main

--- main.py
+++ main.py
@@ -1,44 +1,21 @@
-from trigger_project.models.user import User
-from trigger_project.models.softskill import Softskill
-from trigger_project.models.hardskill import Hardskill
-from trigger_project.models.opening import Opening
-from trigger_project.transformers.opening_transformer import OpeningTransformer
+from trigger_project.test.trigger_test_runner import TriggerTestRunner
 from trigger_project.scoring import TriggerScoringCalculator, TriggerScoringOptions
-from trigger.trigger_interface import TriggerInterface
-from trigger.scoring import ScoringCalculator
+from trigger_project.operation import read_operations
+
+from data.operations_instances_ss_confirmed import fetch_operations_files
+
 from trigger.clusters.ecm import ECM
-from trigger_project.transformers.user_transformer import UserTransformer
 
 if __name__ == "__main__":
 
-    ti = TriggerInterface(ECM(0.5), {
-        "opening": OpeningTransformer(),
-        "user": UserTransformer(),
-    }, TriggerScoringCalculator())
+    operations_files = fetch_operations_files()
 
-    ti.add("1", "opening", Opening(
-        "1",
-        hardSkills=[Hardskill("Hunting"), Hardskill("IT")],
-        softSkills=[Softskill("Teamwork"), Softskill("Makes the dream work")]
+    for operations_file in operations_files:
+        test_runner = TriggerTestRunner(
+            processor_class=ECM,
+            param_grid={ "distance_threshold": [0.5, 1.] },
+            operations=read_operations(operations_file.full_path),
+            scoring_calculator=TriggerScoringCalculator(TriggerScoringOptions(score_to_be_match=0.2)),
+            output_base_folder=f"results/openings_users/operations/{operations_file.test_subpath}/"
         )
-    )
-
-    ti.add("2", "opening", Opening(
-        "2",
-        hardSkills=[Hardskill("IT")],
-        softSkills=[Softskill("Teamwork")]
-        )
-    )
-
-    ti.add("3", "opening", Opening(
-        "3",
-        hardSkills=[Hardskill("Hunter")],
-        softSkills=[Softskill("Dream catcher")]
-        )
-    )
-
-    print(ti.get_scorings_for("user", User(
-        "Kimmy",
-        softSkills=[Softskill("Teamwork")],
-        hardSkills=[Hardskill("Hunter")]
-    )))
+        test_runner.run_tests()
