0a7eeb93a762170ef9bf842154bdb2d523050fad
David Pinto <daxtery11@gmail.com>
2020-12-25 17:55:28 +0000

    Add casts to some operations

--- trigger/trigger_interface.py
+++ trigger/trigger_interface.py
@@ -1,8 +1,9 @@
 from trigger.scoring import TriggerScoring
 from trigger.evaluation.matches import eval_matches
-from typing import List, cast
+from typing import Any, List, Optional, cast
 from interference.interface import Interface
 from interference.operations import CalculateMatchesInfo
+from interference.scoring import Scoring
 
 class TriggerInterface(Interface):
 
@@ -12,3 +13,11 @@ class TriggerInterface(Interface):
         # Don't worry, they are trigger scorings :)
         scorings = cast(List[List[TriggerScoring]], scorings)
         return eval_matches(instances, scorings)
+
+    def get_matches_for(self, transformer_key: str, value: Any) -> Optional[List[TriggerScoring]]:
+        base = super().get_matches_for(transformer_key, value)
+        return cast(Optional[List[TriggerScoring]], base)
+
+    def calculate_scoring_between_value_and_tag(self, transformer_key: str, value: Any, tag: str) -> Optional[TriggerScoring]:
+        base = super().calculate_scoring_between_value_and_tag(transformer_key, value, tag)
+        return cast(Optional[TriggerScoring], base)
